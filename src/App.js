622d165a45434a5c970b5f6eb3f7762775d418e7
Sniper-25 <abdulrahmanahmedalfakih@gmail.com>
2025-03-12 23:40:05 +0300

    Adding project files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/ِAbout/about';
+import ContactUs from './pages/contact/contact';
+import Feed from './pages/Feedback/Feedback';
+import Login from './pages/Login/login';
+import Product from './pages/prodect/prodect';
+import Layout from './Layout';
+import Loc from './pages/Loc/Loc';
+import Footer from './hader_footer/footer';
+import Register from './pages/Login/Register';
+function App() {
+    return (
+
+        <Router>
+            <Layout>
+                <Routes>
+
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/feed' element={<Feed />}></Route>
+                    <Route path='/contact' element={<ContactUs />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                    <Route path='/product' element={<Product />}></Route>
+                    <Route path='/Loc' element={<Loc />}></Route>
+                    <Route path='/foo' element={<Footer />}></Route>
+                    <Route path='/re' element={<Register />}></Route>
+                </Routes>
+            </Layout>
+        </Router>
+
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import Footer from './hader_footer/footer.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <App />
+
+
+);
+
+reportWebVitals();
